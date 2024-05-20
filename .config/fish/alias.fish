6460050926f2f2a9d4dcdd2239fcd8201ce82ce7
zj <80515759+leezhongjun@users.noreply.github.com>
2024-05-20 23:02:01 +0800

    Update alias

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -3,3 +3,16 @@ alias vim="nvim"
 alias oldvim="nvim"
 alias v="nvim"
 alias tmux="tmux -u"
+
+function gc
+    git clone "https://github.com/$argv[1]"
+end
+
+function mc
+    mkdir "$argv[1]" && cd "$argv[1]"
+end
+
+function yuc
+    # yadm update and commit
+    yadm rm -rf --cached . && yadm add ~/ && yadm commit
+end
