af37214739538355b4b2e5348861650ef10079ab
zj <80515759+leezhongjun@users.noreply.github.com>
2024-05-26 18:29:34 +0800

    Add tmux and nvim session saving

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -671,6 +671,16 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		"rmagatti/auto-session",
+		opts = {
+			log_level = "error",
+			auto_session_enable_last_session = true,
+			auto_session_enabled = true,
+			auto_session_create_enabled = true,
+		},
+	},
+
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
 	--
