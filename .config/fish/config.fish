ab898a4fd37efda7d49a436200271ac47d2b538f
zj <80515759+leezhongjun@users.noreply.github.com>
2024-05-28 21:15:30 +0800

    Update fish config to fix tmux rendering issues

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,10 @@
-source ~/.config/fish/alias.fish
-
 if status is-interactive
     # Start tmux
     if not set -q TMUX
-        if tmux has-session -t home
-            exec tmux attach-session -t home
+        if tmux -u has-session -t home
+            exec tmux -u attach-session -t home
         else
-            tmux new-session -s home
+            tmux -u new-session -s home
         end
     end
     source ~/.config/fish/bind.fish
@@ -16,3 +14,5 @@ if status is-interactive
     starship init fish | source
     clear
 end
+
+source ~/.config/fish/alias.fish
