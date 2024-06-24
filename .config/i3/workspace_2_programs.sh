807064bb0aac55e4f15e8cb35afbeefcf553534d
zj <80515759+zj-0@users.noreply.github.com>
2024-06-24 19:49:25 +0800

    Update config

--- .config/i3/workspace_2_programs.sh
+++ .config/i3/workspace_2_programs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-[[ $1 == 0 ]] && cd "/home/zj" && kitty --working-directory=/home/zj/github/perf-eng-6.172/hw/1/matrix-multiply
+[[ $1 == 0 ]] && cd "/home/zj" && kitty --working-directory=/home/zj
