55a961e3e34dc27a6ef40d608e032247b413f700
zj <80515759+zj-0@users.noreply.github.com>
2024-06-29 13:13:02 +0800

    Update config

--- .config/i3/workspace_1_programs.sh
+++ .config/i3/workspace_1_programs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-[[ $1 == 0 ]] && cd "/home/zj" && /usr/bin/python3 /usr/bin/qutebrowser --target=window https://mail.google.com/mail/u/0/
-[[ $1 == 1 ]] && cd "/home/zj" && /usr/bin/python3 /usr/bin/qutebrowser --target=window https://mail.google.com/mail/u/0/
+[[ $1 == 0 ]] && cd "/home/zj" && /usr/bin/python3 /usr/bin/qutebrowser
+[[ $1 == 1 ]] && cd "/home/zj" && /usr/bin/python3 /usr/bin/qutebrowser
---
+++ .config/i3/workspace_2_programs.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+[[ $1 == 0 ]] && cd "/home/zj" && /usr/lib64/firefox/firefox
