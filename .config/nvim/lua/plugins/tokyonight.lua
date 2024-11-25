3564c8d14c2816c65d8d8c33972e707f3e0782ae
Henry Lee <80515759+henrlly@users.noreply.github.com>
2024-11-25 14:40:30 +0800

    Fix tmux theme

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -2,7 +2,7 @@
 local Plugin = { "folke/tokyonight.nvim" }
 Plugin.priority = 1000 -- load this before all other plugins
 Plugin.init = function()
-	vim.cmd.colorscheme("tokyonight-night")
+	vim.cmd.colorscheme("tokyonight")
 	-- You can configure highlights by doing something like:
 	vim.cmd.hi("Comment gui=none")
 end
